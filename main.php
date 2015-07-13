a6ad3a310bf2731e437600fbb3370380a13f19a4
sederap <webmasterjps@hotmail.com>
2015-07-13 08:07:38 -0500

    Revert "10-07-2015"
    This reverts commit 9d86a6f7932ae5270d3c61c4a5bbaee2597b09b5.

--- main.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'include/processes.php';
-$Login_Process = new Login_Process;
-$Login_Process->check_status($_SERVER['SCRIPT_NAME']);
-if($_SESSION['user_level'] >= 5) {
-header( 'Location: '.Script_URL.Script_Path.'admin/index.php' );}
-else {header( 'Location:'.Script_URL.Script_Path.'cliente/index.php');
-}
-?>
